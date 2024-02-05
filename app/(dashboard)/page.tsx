1880645f44fb58adb4ab2f2c769e18d5f49ee397
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-05 16:00:29 +0530

    created all the empty states for boards,favs,search

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
 
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+interface DashboardPageProps {
+  searchParams: {
+    search?: string; // search query inside the search-input.tsx
+    favourites?: string; // favourites query inside the org.tsx
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;

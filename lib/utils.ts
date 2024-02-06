47805a0c6326667ab3a1603fe2cbe6e72948b664
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-06 15:45:46 +0530

    completed participants component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = ["#ccd5ae", "#e9edc9", "#fefae0", "#faedcd", "#d4a373"];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }

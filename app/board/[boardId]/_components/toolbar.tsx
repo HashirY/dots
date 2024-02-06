734f7613a4ed7cd44dd7c0a7ec83a27e756b6417
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-06 14:14:20 +0530

    liveblocks setup

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 export const ToolBar = () => {
@@ -14,5 +15,11 @@ export const ToolBar = () => {
         <div>Redo</div>
       </div>
     </div>
+  );
+};
+
+ToolBar.Skeleton = function ToolBarSkeleton() {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
   );
 };

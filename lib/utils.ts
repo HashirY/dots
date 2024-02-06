66a77b7e69fa6e7efe9c1086fe110f0d1d58e978
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-06 17:57:14 +0530

    completed cursor presence

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
+import { Camera } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-const COLORS = ["#ccd5ae", "#e9edc9", "#fefae0", "#faedcd", "#d4a373"];
+const COLORS = ["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"];
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,4 +10,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
+}
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera
+) {
+  return {
+    x: Math.round(e.clientX - camera.x),
+    y: Math.round(e.clientY - camera.y),
+  };
 }

b88c9d3e32b406a1c13f5e3b9a4f8fc7b0074187
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-07 14:25:24 +0530

    protected liveblocks secret

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -6,8 +6,7 @@ import { api } from "@/convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret:
-    "sk_dev_SCWnehjvjXOdSZsLgE-mb2iIODZuQiHQnWZd-slLpUGywRWK8YHryH7cXwzkkSJs",
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {

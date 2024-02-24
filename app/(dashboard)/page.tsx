93af2e78afc9ab4cd69fc60d96596e5587fc1f81
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-24 20:26:17 +0530

    fixed searching and filtering

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,22 +2,19 @@
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
+import { useSearchParams } from "next/navigation";
 
-interface DashboardPageProps {
-  searchParams: {
-    search?: string; // search query inside the search-input.tsx
-    favourites?: string; // favourites query inside the org.tsx
-  };
-}
-
-const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+const DashboardPage = () => {
   const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || undefined;
+  const favourites = searchParams.get("favorites") || undefined;
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList orgId={organization.id} query={{ search, favourites }} />
       )}
     </div>
   );
